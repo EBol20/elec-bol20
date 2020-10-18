e860ecfc609fc27014092faff1b9eb74712a93b6
EBol20 <lacalabazaaplastada@gmail.com>
2020-10-19 01:05:00 +0200

    translator

--- elec_bol20/R_/scripts/z060R_fun_translator_script.R
+++ elec_bol20/R_/scripts/z060R_fun_translator_script.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 library(data.table)
 
-folder_and_filename = "2020_10_19_00_25/exportacion_EG2020_20201018_181655_4462622834299457053.csv"
+filename = "exportacion_EG2020_20201018_184655_6036973977742226983.csv"
+archive_name = paste0(substr(filename,1,34),".csv")
+name_active = paste0(substr(filename,1,18),"_actual.csv")
 
 translator_vector = c("CODIGO_MESA" = "ID_MESA",
                       "PAIS" = "PAIS",
@@ -24,8 +26,8 @@ translator_vector = c("CODIGO_MESA" = "ID_MESA",
 
 #NUA = #ADN #libre21 #Juntos
 mydata = read.csv(paste0(here::here(),
-                         "/../datos_0_crudos/2020/comp/",
-                         folder_and_filename),colClasses = "character")
+                         "/../datos_0_crudos/2020/comp/", filename),
+                  colClasses = "character")
 names(mydata) = plyr::revalue(names(mydata), translator_vector)
 
 mydata = mydata %>%
@@ -33,6 +35,12 @@ mydata = mydata %>%
   dplyr::mutate(NUA = ADN + LIBRE_21 + JUNTOS)%>%
   mutate(ADN = NULL, LIBRE_21 = NULL, JUNTOS = NULL)
 
+#ARCHIVE DATA
+write.csv(mydata , file = paste0(here::here(),"/../datos_1_intermedios/2020/comp/",
+                                 archive_name),
+          row.names = FALSE, quote = TRUE)
 
-write.csv(mydata , file = paste0(here::here(),"/../datos_1_intermedios/2020/",folder_and_filename),
+#ACTUAL DATA
+write.csv(mydata , file = paste0(here::here(),"/../datos_1_intermedios/2020/comp/",
+                                 name_active),
           row.names = FALSE, quote = TRUE)
