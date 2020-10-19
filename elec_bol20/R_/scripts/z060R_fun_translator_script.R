afd520b9df67f71535b05c76a46229ebd998fa84
EBol20 <lacalabazaaplastada@gmail.com>
2020-10-19 02:37:14 +0200

    first version ID_MESA

--- elec_bol20/R_/scripts/z060R_fun_translator_script.R
+++ elec_bol20/R_/scripts/z060R_fun_translator_script.R
@@ -16,6 +16,18 @@ savepath = paste0(here::here(),"/../datos_1_intermedios/2020/comp/") #path to wh
 
 #function
 translate_and_export_comp_dat = function(filename, file_ext,filepath, savepath){
+  build_mesa_ID =function(pais, paisnum,ciunum, idloc, recnum, mesanum ){
+    mystring = "1"
+    mystring = ifelse(pais == "Bolivia", paste0(mystring,"0"), paste0(mystring,"1"))
+    
+    mystring =paste0(mystring, str_pad(paisnum, 3, pad = "0"))
+    mystring = paste0(mystring, str_pad(ciunum, 3, pad = "0"))
+    mystring = paste0(mystring, str_pad(idloc, 4, pad = "0"))
+    mystring = paste0(mystring, str_pad(recnum, 5, pad = "0"))
+    mystring = paste0(mystring, str_pad(mesanum, 2, pad = "0"))
+    return(mystring)
+  }
+  
   filename = paste0(filename, file_ext)
   archive_name = paste0(substr(filename,1,34),".csv")
   name_active = paste0(substr(filename,1,18),"_actual.csv")
@@ -51,7 +63,9 @@ translate_and_export_comp_dat = function(filename, file_ext,filepath, savepath){
   mydata = mydata %>%
     dplyr::mutate_at(c("CREEMOS","ADN","MAS","FPV","PAN_BOL","LIBRE_21","CC","JUNTOS"),as.numeric)%>%
     dplyr::mutate(NUA = ADN + LIBRE_21 + JUNTOS)%>%
-    mutate(ADN = NULL, LIBRE_21 = NULL, JUNTOS = NULL)
+    mutate(ADN = NULL, LIBRE_21 = NULL, JUNTOS = NULL)%>%
+    dplyr::mutate(ID_MESA = build_mesa_ID(PAIS,ID_PAIS, ID_DEPARTAMENTO, ID_LOCALIDAD,
+                                          ID_RECI, NUMERO_MESA))
   
   #ARCHIVE DATA
   write.csv(mydata , file = paste0(savepath,archive_name),
@@ -64,27 +78,18 @@ translate_and_export_comp_dat = function(filename, file_ext,filepath, savepath){
   return(mydata)
 }#translate_and_export_comp_dat
 
-build_mesa_ID =function(bolivia, paisnum,ciunum, idloc, recnum, mesanum ){
+build_mesa_ID =function(pais, paisnum,ciunum, idloc, recnum, mesanum ){
   mystring = "1"
-  if (bolivia){
-    mystring = paste0(mystring,"0")
-  } else {mystring = paste0(mystring, "1")}
+  mystring = ifelse(pais == "Bolivia", paste0(mystring,"0"), paste0(mystring,"1"))
   
   mystring =paste0(mystring, str_pad(paisnum, 3, pad = "0"))
   mystring = paste0(mystring, str_pad(ciunum, 3, pad = "0"))
   mystring = paste0(mystring, str_pad(idloc, 4, pad = "0"))
   mystring = paste0(mystring, str_pad(recnum, 5, pad = "0"))
-  mystring = paste0(mystring, str_pad(measanum, 2, pad = "0"))
+  mystring = paste0(mystring, str_pad(mesanum, 2, pad = "0"))
   return(mystring)
 }
 
 
 #running the function
 controldata = translate_and_export_comp_dat(filename, file_ext, filepath, savepath)
-
-#%>%
-#   dplyr::mutate(ID_MESA = build_mesa_ID(TRUE, ID_PAIS, ID_DEPARTAMENTO, ID_LOCALIDAD,
-#                                         ID_RECI, NUMERO_MESA ) )
-# test="23"
-# 
-# str_pad(test, 6, pad = "0")
