031ad4541beaab53f14b7e09add25bf4214e5cb8
duett0 <>
2020-10-18 22:51:05 -0300

    added function to get data from 2020 (dataframe) in util

--- elec_bol20/nb_2019/z037_plots_carto.py
+++ elec_bol20/nb_2019/z037_plots_carto.py
@@ -2,16 +2,14 @@ import sys
 sys.path.append(".")
 from elec_bol20 import *
 import elec_bol20.util as ebu
-import elec_bol20.tools as eto
+import elec_bol20.tools as ebt
 
-df0 = ebu.open_combine_2019()
-df0 = ebu.open_combine_2020()
-z = eto.CartoPlots()
-y = z.load_file(df0, _mean=['X', 'Y', 'LAT', 'LON', 'DEN', ], _sum=['HAB', 'CC', 'MAS','CREEMOS', 'FPV','PANBOL','VV'],
+#df0 = ebu.open_combine_2019()
+df0 = ebu.get_dataframe_2020()
+z = ebt.CartoPlots()
+y = z.load_file(df0, _mean=['X', 'Y', 'LAT', 'LON', 'DEN', ], _sum=['HAB', 'CC', 'MAS','CREEMOS', 'FPV','PAN_BOL','VV'],
                 _first=['PAIS', 'REC', 'MUN', 'BOL'])
 
 
-x = z.plot_carto_single(y, 'diff', ebu.P_DIF, name_file='test0.html', low=0, high=100)
-x = z.plot_carto_single(y, 'cc', ebu.P_GRAD_CC, name_file='test0.html', low=0, high=100)
-x = z.plot_carto_single(y, 'mas', ebu.P_GRAD_MAS, name_file='test0.html', low=0, high=100)
+x = z.plot_carto_single(y, 'diff', ebu.P_DIF, name_file='test2020.html', low=0, high=100)
 
