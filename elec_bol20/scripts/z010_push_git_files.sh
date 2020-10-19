13234e8c32ed789c23acb29681deb95e951de8a8
EBol20 <lacalabazaaplastada@gmail.com>
2020-10-19 22:09:54 +0300

    from_mac

--- elec_bol20/scripts/z010_push_git_files.sh
+++ elec_bol20/scripts/z010_push_git_files.sh
@@ -7,17 +7,22 @@ cd $BASEDIR
 cd ../..
 
 echo $(pwd)
+conda activate ebol20
 
 while true; do
 
+python -u ./elec_bol20/nb_2020/z050_plot_contados.py
 
 git add \
 elec_bol20/datos_1_intermedios/2020/comp/*.csv \
-elec_bol20/datos_0_crudos/2020/comp/*.xlsx
+elec_bol20/datos_0_crudos/2020/comp/*.xlsx \
+docs/Ejemplos/z050_panel.html \
 
 git commit -m "auto" \
 elec_bol20/datos_1_intermedios/2020/comp/*.csv \
-elec_bol20/datos_0_crudos/2020/comp/*.xlsx
+elec_bol20/datos_0_crudos/2020/comp/*.xlsx \
+docs/Ejemplos/z050_panel.html \
+
 
 git push origin master
 
